b45879f2a036d7f7d2f7740867f9935ee73180a8
andrea-vega <andrea@entrinsik.com>
2020-10-12 13:19:52 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const path = require('path');
+
+exports.register = function (server, opts, next) {
+
+    const bundle = server.bundle('white-label').scan(__dirname, 'public');
+    server.injector().inject(bundle);
+
+    server.select('content').route({
+        method: 'GET',
+        path: '/assets/white-label/images/{path*}',
+        config: {
+            auth: false,
+            handler: {
+                directory: {
+                    path: path.resolve(__dirname, './public/images'),
+                    listing: false,
+                    index: false
+                }
+            }
+        }
+    });
+
+    server.route({
+        path: '/login.html',
+        method: 'get',
+        config: {
+            auth: false,
+            handler: {
+                file: path.resolve(__dirname, 'assets/login.html')
+            }
+        }
+    });
+
+    server.route({
+        path: '/images/informer.svg',
+        method: 'get',
+        config: {
+            auth: false,
+            handler: {
+                file: path.resolve(__dirname, 'public/images/logo.svg')
+            }
+        }
+    });
+
+    next();
+};
+
+exports.register.attributes = { name: 'inf-white-label' };
